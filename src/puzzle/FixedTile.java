01aa094817e9ebc4cbdab3d902653caf5adb304c
santi-crown <samtigomusik@gmail.com>
2024-10-30 22:27:31 -0500

    Add required classes, every class extends from BaseTile

---
+++ src/puzzle/FixedTile.java
@@ -0,0 +1,8 @@
+package puzzle;
+import shapes.*;
+
+public class FixedTile extends BaseTile{
+    public FixedTile(char label, int xPosition, int yPosition,int row, int column) {
+        super(label, xPosition, yPosition, row, column);                              
+    }
+}
---
+++ src/puzzle/FlyingTile.java
@@ -0,0 +1,8 @@
+package puzzle;
+import shapes.*;
+
+public class FlyingTile extends BaseTile{
+    public FlyingTile(char label, int xPosition, int yPosition,int row, int column) {
+        super(label, xPosition, yPosition, row, column);                              
+    }
+}

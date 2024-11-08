1e6551e252fb450901a0c5ca43dfb281948fafda
AnderProgramming <158221956+AnderssonProgramming@users.noreply.github.com>
2024-11-07 21:11:15 -0500

    Add final code and asta diagram

---
+++ puzzle-cycleInProgress/puzzle/puzzle/puzzle/FixedTile.java
@@ -0,0 +1,42 @@
+package puzzle;
+import java.awt.Color;
+import shapes.*;
+
+
+public class FixedTile extends BaseTile {
+    private Rectangle innerRectangle; // Inside white rectangle  
+
+    public FixedTile(char label, int xPosition, int yPosition, int row, int column) {
+        super(label, xPosition, yPosition, row, column);
+        int innerSize = Tile.SIZE - 10; // To reduce 5 pxs for each side to leave the edge
+        int innerXPosition = this.getXPos() + 5;
+        int innerYPosition = this.getYPos() + 5;
+        innerRectangle = new Rectangle(innerSize, innerSize, Color.WHITE, innerXPosition, innerYPosition);
+        this.makeVisible();
+        innerRectangle.makeVisible(); // Make sure that the drawing is after the red rectangle
+    }
+
+    @Override
+    public void makeVisible(){
+        super.makeVisible();
+        if (innerRectangle != null) innerRectangle.makeVisible();
+    }
+
+    @Override
+    public void makeInvisible(){
+        super.makeInvisible();
+        if (innerRectangle != null) innerRectangle.makeInvisible();
+    }
+
+    @Override
+    public void moveVertical(int distance){
+        super.moveVertical(distance);
+        if (innerRectangle != null) innerRectangle.moveVertical(distance);
+    }
+
+    @Override
+    public void moveHorizontal(int distance){
+        super.moveHorizontal(distance);
+        if (innerRectangle != null) innerRectangle.moveHorizontal(distance);
+    }
+}
